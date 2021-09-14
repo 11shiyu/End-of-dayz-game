7b40388156d106368a1e513f57353215b0f45769
11shiyu <90696374+11shiyu@users.noreply.github.com>
2021-09-14 20:25:11 +0800

    Update a3.py

--- endOfDayz/a3.py
+++ endOfDayz/a3.py
@@ -3,13 +3,9 @@ A GUI-based zombie survival game wherein the player has to reach
 the hospital whilst evading zombies.
 """
 
-# Replace these <strings> with your name, student number and email address.
-__author__ = "<Shiyu Liu>, <45267288>"
-__email__ = "<Shiyu.liu3@uqconnect.edu.au>"
-
 
 import tkinter as tk
-from a2_solution import advanced_game
+from game_support import advanced_game
 from constants import TASK, MAP_FILE
 
 # Uncomment the following imports to import the view classes that represent
